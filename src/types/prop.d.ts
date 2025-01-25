bcd181fbe84335556ed3b6bec00db48b85876985
alparslan <alparslanorgnl@gmail.com>
2025-01-25 16:57:25 +0300

    UI componentler için prop ların tipleri tanımlandı

---
+++ src/types/prop.d.ts
@@ -0,0 +1,14 @@
+export type showErrorPropTypes = {
+    error: string | null
+    errorDetails: string | null
+}
+
+export type emailPropTypes = {
+    email : string | undefined | null 
+    userId : string | undefined | null
+}
+
+export type hamburgerMenuPropTypes = {
+    email : string | undefined | null 
+    userId : string | undefined | null
+}
