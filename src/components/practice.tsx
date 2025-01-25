189630c7ebbfbab6a40030f2ed6d6cea6e68fa65
alparslan <alparslanorgnl@gmail.com>
2025-01-25 23:02:10 +0300

    practice component oluşturuldu

---
+++ src/components/practice.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+//TYPES
+import { practiceComponentPropTypes } from "../types/prop"
+//ASSETS
+import { mitr } from "@/public/fonts"
+
+export default function PracticeComponent({practices, language} : practiceComponentPropTypes) {
+
+    return (
+
+        <>
+            {practices.map((practice,index) => {
+                    return (
+                        <div key={index} className="flex justify-center">
+                            <a href={`/lang?language=${language}&practice=${practice.name}`}>
+                                <button
+                                className={` ${mitr.className} w-64 text-xl mt-5 bg-[#B95DE5] text-white font-medium py-2 rounded-lg shadow-md shadow-[#ad49db] hover:bg-[#ad49db] transition-colors duration-300`}
+                                >
+                                {practice.name}
+                                </button>
+                            </a>
+                        </div>
+                    )
+                })
+            }
+        </>
+    
+    )
+}
