bb6bbe90674f3f33646ea2f3f163e8f1b388420a
alparslan <alparslanorgnl@gmail.com>
2025-01-26 22:48:38 +0300

    CreateLiveSession() action metodu oluşturuldu

--- src/actions/liveSession.ts
+++ src/actions/liveSession.ts
@@ -3,6 +3,26 @@
 //LIBRARIES
 import { prisma } from "../lib/prisma"
 
+export async function CreateLiveSession(sessionId : string, userId : string | undefined | null) {
+
+    try {
+        
+        await prisma.liveSession.create({
+            data: {
+                liveSessionId: sessionId,
+                userId: userId
+            }
+        })
+
+        return {status: 200}
+
+    } catch (error) {
+        
+        if(error instanceof Error) return {status: 500, message: "LiveSession oluşturulurken bir hata oluştu", details: error.message}
+
+    }
+}
+
 export async function DeleteLiveSession(userId : string | undefined | null) {
 
     try {
