489b4d27cafe50a2543b914153b2c665844cbe9b
alparslan <alparslanorgnl@gmail.com>
2025-01-26 21:18:49 +0300

    session provider wrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/public/globals.css"
 import { Navbar } from "../src/components/navbar";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "Language Learn Assistant",
@@ -14,14 +15,16 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <title>{`${metadata.title}`}</title>
         <meta name="description" content={`${metadata.description}`} />
       </head>
-      <body>
-        <div className="container max-w-screen-xl mx-auto px-4">
-            <Navbar></Navbar>
-        </div>
-        <div className="w-full">
-            {children}
-        </div>
-      </body>
+      <SessionProvider>
+        <body>
+          <div className="container max-w-screen-xl mx-auto px-4">
+              <Navbar></Navbar>
+          </div>
+          <div className="w-full">
+              {children}
+          </div>
+        </body>
+      </SessionProvider>
     </html>
   )
 }
