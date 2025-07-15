9a67b6730450e1d9ecd96c71d1836f3175ef2d9c
alparslan <alparslanorgnl@gmail.com>
2025-07-15 14:57:56 +0300

    Başla butonuna tıklandığında dil seçilmediyse alert vermesi sağlandı

--- src/components/homePage/flag.tsx
+++ src/components/homePage/flag.tsx
@@ -1,24 +1,35 @@
 "use client"
 
 //REACT & NEXT
-import Link from "next/link"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 //TYPES
 import { flagComponentPropTypes } from "../../types/prop"
 //ASSETS
 import { mitr } from "@/public/fonts"
 
+
 export default function FlagComponent({languages} : flagComponentPropTypes) {
 
     //STATES
     const [selected, setSelected] = useState<string>("")
-    const [buttonActive, setButtonActive] = useState<boolean>(false)
+    const router = useRouter()
+
 
     //HANDLE CLICK
     const handleClick = (language : any) => {
         setSelected(language.name)
-        setButtonActive(true)
+    }
+
+    // HANDLE START BUTTON
+    const handleStart = () => {
+        if (!selected) {
+            alert("Lütfen bir dil seçiniz.")
+            return
+        }
+        router
+        router.push(`/lang?language=${selected}`)
     }
 
     return (
@@ -43,14 +54,12 @@ export default function FlagComponent({languages} : flagComponentPropTypes) {
                 }
             </div>
             <div className="flex justify-center mt-4">
-                <Link href={`/lang?language=${selected}`}>
-                    <button
-                    className={` ${mitr.className} mt-20 bg-[#58CC02] text-white font-medium py-3 px-20 rounded-lg shadow-md shadow-[#58A700] hover:bg-[#58A700] transition-colors duration-300`}
-                    disabled={!buttonActive}
-                    >
-                    BAŞLA
-                    </button>
-                </Link>
+                <button
+                onClick={handleStart}
+                className={` ${mitr.className} mt-20 bg-[#58CC02] text-white font-medium py-3 px-20 rounded-lg shadow-md shadow-[#58A700] hover:bg-[#58A700] transition-colors duration-300`}
+                >
+                BAŞLA
+                </button>
             </div>
         </>
     )
