4e835d3198648284b6c00e8d76612949108d5fdd
alparslan <alparslanorgnl@gmail.com>
2025-09-24 23:08:42 +0300

    socket_io_refactor_1

--- src/lib/socketClient.ts
+++ src/lib/socketClient.ts
@@ -6,7 +6,7 @@ const port = parseInt(process.env.PORT || "3000", 10)
 
 const URL = process.env.NEXT_PUBLIC_SOCKET_URL || `http://${hostname}:${port}`
 
-const socket = (globalThis as any).socket || io(URL, { autoConnect: true })
+const socket = (globalThis as any).socket || io(URL, { autoConnect: true , closeOnBeforeunload : true})
 ;(globalThis as any).socket = socket
 
 export default socket
