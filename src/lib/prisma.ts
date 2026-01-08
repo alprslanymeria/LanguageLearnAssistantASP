a1bc01b86fedd6d374c5fe9af5e3e03206ea8229
alparslan <alparslanorgnl@gmail.com>
2026-01-08 23:40:57 +0300

    Prisma schema files have been separated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,28 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaMssql } from '@prisma/adapter-mssql';
+import { PrismaClient } from '../generated/prisma/client';
 
-declare global {
-  
-  var prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const sqlConfig = {
+
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  server: process.env.HOST,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true,
+    trustServerCertificate: true
+  }
 }
 
-export const prisma = globalThis.prisma || new PrismaClient()
+const adapter = new PrismaMssql(sqlConfig)
 
-globalThis.prisma = prisma
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
