9675e73b83c0c6402da69acd09fd4741b28f7058
alparslan <alparslanorgnl@gmail.com>
2026-02-12 14:56:07 +0300

    removed storage

--- src/infrastructure/storage/IStorageFactory.ts
+++
@@ -1,8 +0,0 @@
-// IMPORTS
-import { IStorageStrategy } from "@/src/infrastructure/storage/IStorageStrategy"
-import { StorageType } from "@/src/infrastructure/storage/Storage"
-
-export interface IStorageFactory {
-
-    createStrategy(type: StorageType): IStorageStrategy
-}
