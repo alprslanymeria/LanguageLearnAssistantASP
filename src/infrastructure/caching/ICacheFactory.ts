ed2162874e0586effcb3b2e8917b8a703ac51f59
alparslan <alparslanorgnl@gmail.com>
2026-02-12 14:54:37 +0300

    removed caching

--- src/infrastructure/caching/ICacheFactory.ts
+++
@@ -1,8 +0,0 @@
-// IMPORTS
-import { CacheType } from "@/src/infrastructure/caching/Cache"
-import { ICacheStrategy } from "@/src/infrastructure/caching/ICacheStrategy"
-
-export interface ICacheFactory {
-
-    createStrategy(type: CacheType): ICacheStrategy
-}
--- src/infrastructure/caching/ICacheService.ts
+++
@@ -1,8 +0,0 @@
-// FACADE DESIGN PATTERN INTERFACE FOR CACHING
-export interface ICacheService {
-    
-    getOrSet<T>(key: string, fetcher: () => Promise<T>, ttlSeconds?: number): Promise<T>
-    invalidate(keys: string | string[]): Promise<void>
-    invalidateByPrefix(prefix: string): Promise<void>
-    withInvalidation<T>(keysToInvalidate: string | string[], action: () => Promise<T>): Promise<T>
-}
