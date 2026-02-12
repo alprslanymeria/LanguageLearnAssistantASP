3d4daa96fa2b0441fe6090bd90daa719970435f7
alparslan <alparslanorgnl@gmail.com>
2026-02-12 15:01:00 +0300

    removed OTEL

--- instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-
-    if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.USE_OPEN_TELEMETRY === 'true') {
-        
-        await import('./instrumentation.node')
-    }
-}
